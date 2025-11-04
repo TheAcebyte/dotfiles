4add18a6e04212c523778e2f3d81da7f5fd983d8
Acebyte <theacebyte@gmail.com>
2025-11-05 00:52:05 +0100

    Fix border colors on float docs in Neovim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,45 +1,62 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-file-browser.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    config = function()
-      local telescope = require("telescope")
-      telescope.setup({
-        extensions = {
-          file_browser = {
-            theme = "ivy",
-            display_stat = { date = true, size = true },
-            grouped = true,
-            dir_icon = "",
-            initial_mode = "normal",
-            path = "%:p:h",
-            cwd_to_path = true,
-            hidden = true,
-          },
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      telescope.load_extension("file_browser")
-      telescope.load_extension("ui-select")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					preview = {
+						treesitter = true,
+					},
+					layout_config = {
+						horizontal = {
+							preview_width = 0.5,
+							preview_cutoff = 0,
+						},
+						width = 0.85,
+						height = 0.85,
+					},
+				},
+				extensions = {
+					file_browser = {
+						theme = "ivy",
+						display_stat = { date = true, size = true },
+						grouped = true,
+						dir_icon = "",
+						initial_mode = "normal",
+						path = "%:p:h",
+						cwd_to_path = true,
+						hidden = true,
+						layout_config = {
+							preview_width = 0.5,
+							preview_cutoff = 0,
+						},
+					},
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			telescope.load_extension("file_browser")
+			telescope.load_extension("ui-select")
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-      vim.keymap.set(
-        "n",
-        "<leader>fs",
-        telescope.extensions.file_browser.file_browser,
-        { desc = "Telescope file system" }
-      )
-    end,
-  },
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+			vim.keymap.set(
+				"n",
+				"<leader>fs",
+				telescope.extensions.file_browser.file_browser,
+				{ desc = "Telescope file system" }
+			)
+		end,
+	},
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,74 +1,77 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        integrations = {
-          telescope = {
-            enabled = true,
-          },
-        },
-        custom_highlights = function(colors)
-          return {
-            TelescopeNormal = { bg = colors.base },
-            TelescopeBorder = { bg = colors.base, fg = colors.blue },
-            TelescopePromptTitle = { bg = colors.base },
-            TelescopePreviewTitle = { bg = colors.base },
-            TelescopeResultsTitle = { bg = colors.base },
-          }
-        end,
-      })
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup()
-    end,
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-          documentation = {
-            opts = {
-              win_options = {
-                winhighlight = "NormalFloat:Normal",
-              },
-            },
-          },
-        },
-        presets = {
-          bottom_search = true,
-          command_palette = true,
-          long_message_to_split = true,
-          inc_rename = false,
-          lsp_doc_border = true,
-        },
-        routes = {
-          {
-            filter = {
-              event = "lsp",
-              kind = "progress",
-              find = "jdtls",
-            },
-            opts = { skip = true },
-          },
-        },
-      })
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					telescope = {
+						enabled = true,
+					},
+				},
+				custom_highlights = function(colors)
+					return {
+						TelescopeNormal = { bg = colors.base },
+						TelescopeBorder = { bg = colors.base, fg = colors.blue },
+						TelescopePromptTitle = { bg = colors.base },
+						TelescopePreviewTitle = { bg = colors.base },
+						TelescopeResultsTitle = { bg = colors.base },
+
+						NormalFloat = { bg = colors.base },
+						FloatBorder = { bg = colors.base, fg = colors.blue },
+					}
+				end,
+			})
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {},
+		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+		config = function()
+			require("noice").setup({
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+					documentation = {
+						opts = {
+							win_options = {
+								winhighlight = "NormalFloat:Normal",
+							},
+						},
+					},
+				},
+				presets = {
+					bottom_search = true,
+					command_palette = true,
+					long_message_to_split = true,
+					inc_rename = false,
+					lsp_doc_border = true,
+				},
+				routes = {
+					{
+						filter = {
+							event = "lsp",
+							kind = "progress",
+							find = "jdtls",
+						},
+						opts = { skip = true },
+					},
+				},
+			})
+		end,
+	},
 }
