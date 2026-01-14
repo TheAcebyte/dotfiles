097c57cfd9b6bdc7abe7720c69adfaec2ef5853f
acebyte <theacebyte@gmail.com>
2026-01-15 00:08:42 +0100

    Replace ts_ls with vtsls

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- add your LSP servers here
-local servers = { "clangd", "cssls", "html", "ts_ls" }
+local servers = { "clangd", "cssls", "html", "vtsls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
