15fb1cd9e42784bb1dfa3e7396d9256af3c581d8
acebyte <theacebyte@gmail.com>
2026-01-14 04:00:44 +0100

    Add LSP/Treesitter packages for JavaScript and TypeScript

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- add your LSP servers here
-local servers = { "clangd", "cssls", "html" }
+local servers = { "clangd", "cssls", "html", "ts_ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
