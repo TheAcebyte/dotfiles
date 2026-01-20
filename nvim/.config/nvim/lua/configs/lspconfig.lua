fffdf61265d7149b6076d81aed08f70cd9705232
Acebyte <theacebyte@gmail.com>
2026-01-20 10:30:23 +0100

    Add Mason/Treesitter packages for Java

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- add your LSP servers here
-local servers = { "clangd", "cssls", "html", "vtsls" }
+local servers = { "clangd", "cssls", "html", "jdtls", "vtsls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
